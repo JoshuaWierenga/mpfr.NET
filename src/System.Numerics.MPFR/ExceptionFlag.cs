6b0ba888caebd2710cda31d61ce1d37f1b8ba284
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2020-07-08 14:26:53 +0000

    Updated BigFloat Functions
    Added new functions from mpfr 4.

---
+++ src/System.Numerics.MPFR/ExceptionFlag.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Numerics.MPFR
+{
+    public enum ExceptionFlag
+    {
+        Underflow = 1,
+        Overflow = 2,
+        NotANumber = 4,
+        Inexact = 8,
+        ERange = 16,
+        DivisionByZero = 32,
+        AllFlags = 63
+    }
+}
